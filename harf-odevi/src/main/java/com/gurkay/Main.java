52209347c108bc2be4b8d069f7e38095149f0e99
Gürkay Çilinger <154504814+Gurkayx@users.noreply.github.com>
2025-01-08 19:27:11 +0300

    Update Main.java

--- harf-odevi/src/main/java/com/gurkay/Main.java
+++ harf-odevi/src/main/java/com/gurkay/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
         //�smi yazdıralım
         AsciiArtPrinter asciiArtPrinter = new AsciiArtPrinter();
-        asciiArtPrinter.yazdir("ERDINC");
+        asciiArtPrinter.yazdir("GURKAY");
     }
 }
